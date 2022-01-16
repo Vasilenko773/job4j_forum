c24d2b8d79089244033d7c6a9c5ca7ececbc0c40
Нина <vasilenko.lesnik2017@yandex.ru>
2022-01-16 18:08:16 +0900

    0. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/forum/Main.java
+++ src/main/java/ru/job4j/forum/Main.java
@@ -1,10 +1,14 @@
 package ru.job4j.forum;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import ru.job4j.forum.config.WebSecurity;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
@@ -13,6 +17,16 @@ public class Main extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Main.class, WebSecurity.class);
     }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
